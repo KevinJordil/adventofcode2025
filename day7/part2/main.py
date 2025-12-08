78bb201b39bf074526387833f5e0f05e4b8fe173
Kevin Jordil <kevin.jordil@outlook.com>
2025-12-08 16:22:01 +0100

    Fix error

--- day7/part2/main.py
+++ day7/part2/main.py
@@ -1,15 +1,14 @@
 grid = []
 
-with open("input.txt", "r") as f:
+with open("input2.txt", "r") as f:
     for line in f:
         line = line.rstrip("\n")
         row = list(line)
         grid.append(row)
 
-for row in grid:
-    print(" ".join(row))
-    
-path_number = 0
+#for row in grid:
+    #print(" ".join(row))
+
     
 for row in range(len(grid)):
     for col in range(len(grid[row])):
@@ -24,6 +23,7 @@ for row in range(len(grid)):
             if col + 1 < len(grid[row]) and grid[row][col + 1] != "|":
                 grid[row][col + 1] = "|"
                 
+                
             if row + 1 < len(grid):
                 if col - 1 >= 0 and grid[row + 1][col - 1] != "|":
                     grid[row + 1][col - 1] = "|"
@@ -36,9 +36,40 @@ for row in range(len(grid)):
             if row + 1 < len(grid) and grid[row + 1][col] == ".":
                 grid[row + 1][col] = "|"
 
-                
-print("Result")
-for row in grid:
-    print(" ".join(row))
+s_pos_row = 0
+s_pos_col = 0
 
-print(path_number)
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        if grid[row][col] == "^" and grid[row - 1][col] != "|":
+            grid[row][col] = "."
+        elif grid[row][col] == "S":
+            s_pos_row = row
+            s_pos_col = col
+                
+
+
+rows = len(grid)
+cols = len(grid[0])
+curr_rays = {s_pos_col: 1}
+
+for row in range(s_pos_row, rows):
+    next_rays = {}
+    for col, weight in curr_rays.items():
+        cell = grid[row][col]
+        
+        if cell in ['.', 'S', '|']:
+            next_rays[col] = next_rays.get(col, 0) + weight
+            
+        elif cell == '^':
+            left = col - 1
+            if grid[row][left] == '|':
+                next_rays[left] = next_rays.get(left, 0) + weight
+
+            right = col + 1
+            if grid[row][right] == '|':
+                next_rays[right] = next_rays.get(right, 0) + weight
+
+    curr_rays = next_rays
+
+print("count:", sum(curr_rays.values()))
